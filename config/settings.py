8d0944bd2572c3270f67c516bfdd3ba5b4012ecb
ArtAlSh <105551538+ArtAlSh@users.noreply.github.com>
2022-12-25 23:56:27 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = ['shielded-bastion-12344.herokuapp.com', '127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = [os.environ.get('HOST'), '127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definition
